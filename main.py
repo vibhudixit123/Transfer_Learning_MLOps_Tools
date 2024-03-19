332b73cb304e4f0432d1d1c5622039f77d3f5ecf
vibhu dixit <dixitvibhu20@gmail.com>
2024-03-19 17:15:34 +0530

    training stage updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.Classifier_Model import logger
 from src.Classifier_Model.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.Classifier_Model.pipeline.base_model_pipeline import PrepareBaseModelTraningPipeline
-
+from src.Classifier_Model.pipeline.training_model_pipeline import PrepareTrainingModelPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -24,3 +24,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Training Model stage"
+try:
+        logger.info(f"*********************")
+        logger.info(f">>>>>stage{STAGE_NAME} started<<<<<<<<")
+        obj = PrepareTrainingModelPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>stage{STAGE_NAME} completed <<<<<<<<<<\n\nx=============x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
